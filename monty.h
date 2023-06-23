a2d730f44e6af941bac8ea783b38dca6eeaad60b
THOSSYN <alextosin5@gmail.com>
2023-06-22 18:29:06 -0700

    Fix: header file

--- monty.h
+++ monty.h
@@ -78,6 +78,6 @@ void pchar(stack_t **stack, unsigned int line_number);
 void pstr(stack_t **stack, unsigned int line_number);
 void swap(stack_t **stack, unsigned int line_number);
 void rotl(stack_t **stack, unsigned int line_number);
-void rotr(stack_t **stack, un
+void rotr(stack_t **stack, uns
 
 #endif /* MONTY_H */
